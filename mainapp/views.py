0bf9b56181640d085067b46488bc47913e6c736d
iTheSand <thesand2009@yandex.ru>
2021-03-06 23:19:58 +0300

    lesson_2_hw

---
+++ mainapp/views.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+
+from django.shortcuts import render
+
+
+# Create your views here.
+def main(request):
+    content = {
+        'title': 'Главная'
+    }
+
+    return render(request, 'mainapp/index.html', content)
+
+
+def products(request):
+    links_menu = [
+        {'href': 'products_all', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'href': 'products_modern', 'name': 'модерн'},
+        {'href': 'products_classic', 'name': 'классика'}
+    ]
+    content = {
+        'title': 'Продукты',
+        'links_menu': links_menu,
+        'special_offer': datetime.now()
+    }
+    return render(request, 'mainapp/products.html', content)
+
+
+def contact(request):
+    content = {
+        'title': 'Контакты'
+    }
+    return render(request, 'mainapp/contact.html', content)
