247a20ca9c64289e25b10e168e295de71ca44781
iTheSand <thesand2009@yandex.ru>
2021-03-08 15:52:29 +0300

    lesson_3_hw

---
+++ geekshop/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+import mainapp.views as mainapp
+
+urlpatterns = [
+    path('', mainapp.main, name='main'),
+    path('products/', include('mainapp.urls', namespace='products')),
+    path('contact/', mainapp.contact, name='contact'),
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
