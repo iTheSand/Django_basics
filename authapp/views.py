708c079a42d6626b767842a1f9ec70682f28f365
iTheSand <thesand2009@yandex.ru>
2021-03-24 21:01:35 +0300

    lesson_7_hw

---
+++ authapp/views.py
@@ -0,0 +1,70 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
+
+
+def login(request):
+    login_form = ShopUserLoginForm(data=request.POST or None)
+
+    next = request.GET['next'] if 'next' in request.GET.keys() else ''
+
+    if request.method == 'POST' and login_form.is_valid():
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            if 'next' in request.POST.keys():
+                return HttpResponseRedirect(request.POST['next'])
+            else:
+                return HttpResponseRedirect(reverse('main'))
+
+    content = {
+        'title': 'вход',
+        'login_form': login_form,
+        'next': next
+    }
+
+    return render(request, 'authapp/login.html', content)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('main'))
+
+
+def register(request):
+    if request.method == 'POST':
+        register_form = ShopUserRegisterForm(request.POST, request.FILES)
+        if register_form.is_valid():
+            register_form.save()
+            return HttpResponseRedirect(reverse('auth:login'))
+    else:
+        register_form = ShopUserRegisterForm()
+
+    content = {
+        'title': 'регистрация',
+        'form': register_form
+    }
+    return render(request, 'authapp/register.html', content)
+
+
+def edit(request):
+    if request.method == 'POST':
+        edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('auth:edit'))
+    else:
+        edit_form = ShopUserEditForm(instance=request.user)
+
+    content = {
+        'title': 'редактирование',
+        'edit_form': edit_form
+    }
+
+    return render(request, 'authapp/edit.html', content)
